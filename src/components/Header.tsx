4d019e0e2ac25d89605557f0eaea290f76278fa7
m0bar3z <amobarez00@gmail.com>
2024-04-10 17:42:04 +0330

    feat: navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,9 @@
-import Link from "next/link";
+import NavBar from "./Navbar";
 
 export default async function Header() {
   return (
     <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/" aria-label="Home Page">
-              Ali Mobarezi
-            </Link>
-          </li>
-          <li>
-            <Link href="about" aria-label="About Page">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="blog" aria-label="Blog Page">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link href="projects" aria-label="Projects Page">
-              Projects
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
     </header>
   );
 }
